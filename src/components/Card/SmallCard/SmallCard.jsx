15da0a3ff24ad443c72cd5bdf21bc6a43b2b10f0
Rachel Llyod <rachel.lloyd@MacBook-Pro.local>
2023-08-07 15:48:04 -0400

    added carousel code to small card

--- src/components/Card/SmallCard/SmallCard.jsx
+++ src/components/Card/SmallCard/SmallCard.jsx
@@ -9,11 +9,41 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { Typography } from "@mui/material";
 
+// CAROUSELL COMPONENT
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
 export default function SmallCard({id, newDate, title, desc, photoData, video}) {
     let oldDate = new Date(newDate)
     let oldDateMonth = oldDate.toLocaleDateString("default", {month:'long'})
     let oldDateYear = oldDate.toLocaleDateString("default", {year:'numeric'})
     let reformattedDate = oldDateMonth + " " + oldDateYear
+
+    // Carousell Functions
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = photoData.length;
+
+    const handleNext = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+      };
+    
+      const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+      };
+    
+      const handleStepChange = (step) => {
+        setActiveStep(step);
+      };
     
     return(<>
     <div className="v_line"/>
@@ -27,6 +57,72 @@ export default function SmallCard({id, newDate, title, desc, photoData, video}) 
         <AccordionDetails>
             <Typography variant="body2" className="accordDesc">{desc}</Typography>
             <ChildModal photoData={photoData}/>
+            {/* <Box sx={{ maxWidth: 400, flexGrow: 1}}>
+            <Paper
+                square
+                elevation={0}
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    height: 50,
+                    pl: 2,
+                    bgcolor: 'background.default',
+                }}
+            />
+            <AutoPlaySwipeableViews
+                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                index = {activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents
+            >
+                {photoData.map((step, index) => (
+                    <div key={step.photoId}>
+                        {Math.abs(activeStep - index) <= 2 ? (
+                            <Box
+                                component="img"
+                                sx = {{
+                                    height: 225,
+                                    display: 'block',
+                                    maxWidth: 400,
+                                    overflow: 'hidden',
+                                    width: '100%',
+                                }}
+                                src={step.url}
+                            />
+                        ) : null}
+                    </div>
+                ))}
+            </AutoPlaySwipeableViews>
+            <MobileStepper
+                steps={maxSteps}
+                position='static'
+                activeStep={activeStep}
+                nextButton={
+                    <Button
+                        size='small'
+                        onClick={handleNext}
+                        disabled={activeStep === maxSteps - 1}
+                    >
+                        Next 
+                        {theme.direction === 'rtl' ? (
+                            <KeyboardArrowLeft />
+                        ) : (
+                            <KeyboardArrowRight />
+                        )}
+                    </Button>
+                }
+                backButton={
+                    <Button size='small' onClick={handleBack} disabled={activeStep === 0}>
+                        {theme.direction === 'rtl' ? (
+                            <KeyboardArrowRight />
+                        ) : (
+                            <KeyboardArrowLeft />
+                        )}
+                        Back
+                    </Button>
+                }
+            />
+            </Box> */}
         </AccordionDetails>
     </Accordion>
     </>)
